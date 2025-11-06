192aadafe81bf595dff66a5e0411626233930aa2
nisakhya-cloudops <knisakhya@gmail.com>
2025-11-06 13:19:48 -0600

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,47 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "tf-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  for_each = toset(var.public_subnets)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value
+  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, 0)
+
+  tags = {
+    Name = "public-${each.value}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  for_each = toset(var.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value
+  availability_zone = element(data.aws_availability_zones.available.names, 1)
+
+  tags = {
+    Name = "private-${each.value}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
